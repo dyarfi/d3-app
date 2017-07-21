b0f2814caf8455b74979b8a5ea04f313899eb258
dyarfi <defrian.yarfi@gmail.com>
2017-07-21 13:55:24 +0700

    Banner routes and Image View

--- app/Modules/Banner/routes.php
+++ app/Modules/Banner/routes.php
@@ -22,17 +22,17 @@ Route::group(['prefix' => config('setting.admin_url')], function()
     // Portfolios DataTables routes
     Route::get('banner/datatable', ['as'=>'admin.banners.datatable','uses'=>'App\Modules\Banner\Controller\Banners@datatable']);
     // banners Controller routes
-    Route::get('banner', ['as'=>'admin.banners.index','uses'=>'App\Modules\Banner\Controller\banners@index']);
+    Route::get('banner', ['as'=>'admin.banners.index','uses'=>'App\Modules\Banner\Controller\Banners@index']);
     Route::get('banner/export', ['as'=>'admin.banners.export','uses'=>'App\Modules\Banner\Controller\Banners@export']);
-    Route::get('banner/create', ['as'=>'admin.banners.create','uses'=>'App\Modules\Banner\Controller\banners@create']);
-    Route::post('banner/create', ['as'=>'admin.banners.store','uses'=>'App\Modules\Banner\Controller\banners@store']);
-    Route::post('banner/change', ['as'=>'admin.banners.change','uses'=>'App\Modules\Banner\Controller\banners@change']);
+    Route::get('banner/create', ['as'=>'admin.banners.create','uses'=>'App\Modules\Banner\Controller\Banners@create']);
+    Route::post('banner/create', ['as'=>'admin.banners.store','uses'=>'App\Modules\Banner\Controller\Banners@store']);
+    Route::post('banner/change', ['as'=>'admin.banners.change','uses'=>'App\Modules\Banner\Controller\Banners@change']);
     // Put the method with the parameter below the static method
-    Route::get('banner/{id}/show', ['as'=>'admin.banners.show', 'uses'=>'App\Modules\Banner\Controller\banners@show']);
-    Route::get('banner/{id}', ['as'=>'admin.banners.edit','uses'=>'App\Modules\Banner\Controller\banners@edit']);
-    Route::post('banner/{id}', ['as'=>'admin.banners.update','uses'=>'App\Modules\Banner\Controller\banners@update']);
-    Route::get('banner/{id}/trash', ['as'=>'admin.banners.trash','uses'=>'App\Modules\Banner\Controller\banners@trash']);
-    Route::get('banner/{id}/restored', ['as'=>'admin.banners.restored','uses'=>'App\Modules\Banner\Controller\banners@restored']);
-    Route::get('banner/{id}/delete', ['as'=>'admin.banners.delete','uses'=>'App\Modules\Banner\Controller\banners@delete']);
+    Route::get('banner/{id}/show', ['as'=>'admin.banners.show', 'uses'=>'App\Modules\Banner\Controller\Banners@show']);
+    Route::get('banner/{id}', ['as'=>'admin.banners.edit','uses'=>'App\Modules\Banner\Controller\Banners@edit']);
+    Route::post('banner/{id}', ['as'=>'admin.banners.update','uses'=>'App\Modules\Banner\Controller\Banners@update']);
+    Route::get('banner/{id}/trash', ['as'=>'admin.banners.trash','uses'=>'App\Modules\Banner\Controller\Banners@trash']);
+    Route::get('banner/{id}/restored', ['as'=>'admin.banners.restored','uses'=>'App\Modules\Banner\Controller\Banners@restored']);
+    Route::get('banner/{id}/delete', ['as'=>'admin.banners.delete','uses'=>'App\Modules\Banner\Controller\Banners@delete']);
 
 });
