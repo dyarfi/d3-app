9af429a1bdd5bd952bfb0f9bde76f00ac10e2260
dyarfi <defrian.yarfi@gmail.com>
2017-10-24 14:08:11 +0700

    Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,12 +3,13 @@
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 use View;
 
 abstract class Controller extends BaseController {
 
-	use DispatchesJobs, ValidatesRequests;
+	use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
 	/**
 	* Master layout
